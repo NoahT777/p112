93c8b78aff29a3f7c27deb973f18e0e98ec3a9a1
Noah Thomas <92344495+NoahT777@users.noreply.github.com>
2022-06-24 10:02:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,95 @@
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality : 90
+
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot(){
+
+    Webcam.snap(function(data_uri){
+
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "' + data_uri + '"/>';
+
+    });
+
+}
+
+console.log("ml5.version", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/otp66BBep/model.json", modelLoaded);
+
+function modelLoaded(){
+
+    console.log("Model Loaded!");
+
+}
+
+
+function check(){
+
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+
+}
+
+function gotResult(error, results){
+
+    if(error){
+
+        console.error(error);
+
+    } else{
+
+        console.log(results);
+
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+
+        if(results[0].label == "Peace"){
+
+            document.getElementById("update_emoji").innerHTML = "✌️";
+
+        }
+        if (results[0].label == "Thumbs Up"){
+
+            document.getElementById("update_emoji").innerHTML = "👍";
+
+        }
+        if (results[0].label == "Okay"){
+
+            document.getElementById("update_emoji").innerHTML = "👌";
+
+        }
+        
+        if (results[1].label == "Peace"){
+
+            document.getElementById("update_emoji2").innerHTML = "✌️";
+        }
+        if (results[1].label == "Thumbs Up"){
+
+            document.getElementById("update_emoji2").innerHTML = "👍";
+
+        }
+        if (results[1].label == "Okay"){
+
+            document.getElementById("update_emoji2").innerHTML = "👌";
+
+        }
+
+    }
+
+}
